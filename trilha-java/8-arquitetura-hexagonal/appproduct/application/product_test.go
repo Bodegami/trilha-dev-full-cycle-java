6181520f8e2d7170a825d9fc6109855346ac66c5
Renato <rena.f.oliveira@gmail.com>
2025-01-25 20:18:27 -0300

    implementando e testando demais metodos

--- trilha-java/8-arquitetura-hexagonal/appproduct/application/product_test.go
+++ trilha-java/8-arquitetura-hexagonal/appproduct/application/product_test.go
@@ -2,6 +2,7 @@ package application_test
 
 import (
 	"github.com/bodegami/trilha-dev-full-cycle-java/arquitetura-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -18,4 +19,87 @@ func TestProduct_Enable(t *testing.T) {
 	product.Price = 0
 	err = product.Enable()
 	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disable", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+}
+
+func TestProduct_GetId(t *testing.T) {
+	product := application.Product{}
+	id := uuid.NewV4().String()
+
+	product.ID = id
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	result := product.GetId()
+	require.Equal(t, id, result)
+}
+
+func TestProduct_GetName(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	result := product.GetName()
+	require.Equal(t, "Hello", result)
+}
+
+func TestProduct_GetStatus(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	result := product.GetStatus()
+	require.Equal(t, application.DISABLED, result)
+}
+
+func TestProduct_GetPrice(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	result := product.GetPrice()
+	require.Equal(t, 10.0, result)
 }
