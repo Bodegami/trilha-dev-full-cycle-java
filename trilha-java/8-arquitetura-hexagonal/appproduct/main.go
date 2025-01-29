22dca4bda83f33c8755c597a63816b5161aaed52
Renato <rena.f.oliveira@gmail.com>
2025-01-28 23:36:34 -0300

    Implementando CLI com Cobra

--- trilha-java/8-arquitetura-hexagonal/appproduct/main.go
+++ trilha-java/8-arquitetura-hexagonal/appproduct/main.go
@@ -1,19 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-	db2 "github.com/bodegami/trilha-dev-full-cycle-java/arquitetura-hexagonal/adapter/db"
-	"github.com/bodegami/trilha-dev-full-cycle-java/arquitetura-hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/bodegami/trilha-dev-full-cycle-java/arquitetura-hexagonal/cmd"
 
 func main() {
-	var db *sql.DB
-	db, _ = sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-
-	product, _ := productService.Create("Product Example", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
