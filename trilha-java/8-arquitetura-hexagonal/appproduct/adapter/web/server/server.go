a3cbe988a1277b085d06ce5eb1e1a31ff02ff34b
Renato <rena.f.oliveira@gmail.com>
2025-01-30 23:18:05 -0300

    criando um servidor web

---
+++ trilha-java/8-arquitetura-hexagonal/appproduct/adapter/web/server/server.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"github.com/bodegami/trilha-dev-full-cycle-java/arquitetura-hexagonal/application"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebServer(service application.ProductServiceInterface) *WebServer {
+	return &WebServer{service}
+}
+
+func (w *WebServer) Serve() {
+
+	router := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":8080",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.LstdFlags),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
